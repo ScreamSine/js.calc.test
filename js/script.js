49e190374835a6c4e50ef65098d4947929adce2c
vadim <vadimir29@gmail.com>
2022-05-03 15:13:28 +0200

    Second commit

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
 let buttonAdd = document.querySelector(".add");
 let buttonSubtract = document.querySelector(".subtract");
+let buttonDivine = document.querySelector(".divine");
+let buttonMylty = document.querySelector(".multy");
 let buttonClear = document.querySelector(".clear");
 
 let num1 = document.querySelector(".num1");
 let num2 = document.querySelector(".num2");
+let result = document.querySelector(".result");
 
 buttonAdd.addEventListener("click", function () {
-  num1 = document.querySelector(".num1").value;
-  num2 = document.querySelector(".num2").value;
-  alert(parseInt(num1) + parseInt(num2));
+  result.value = parseInt(num1.value) + parseInt(num2.value);
 });
+
 buttonSubtract.addEventListener("click", function () {
-  num1 = document.querySelector(".num1").value;
-  num2 = document.querySelector(".num2").value;
-  alert(num1 - num2);
+  result.value = num1.value - num2.value;
+});
+
+buttonDivine.addEventListener("click", function () {
+  result.value = num1.value / num2.value;
+});
+
+buttonMylty.addEventListener("click", function () {
+  result.value = num1.value * num2.value;
 });
 
 buttonClear.addEventListener("click", function () {
-  document.querySelector(".num1").value = 0;
-  document.querySelector(".num2").value = 0;
+  num1.value = 0;
+  num2.value = 0;
+  result.value = 0;
 });
